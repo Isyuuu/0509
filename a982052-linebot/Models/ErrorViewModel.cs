78678e70b92bd2408fc6e3d5f689957210c15ec5
Isyuuu <yus985564@gmail.com>
2023-05-09 15:33:18 +0800

    init

---
+++ a982052-linebot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace a982052_linebot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
