2dda39e1f5b0e0c88b12c23743307c36b152fb50
xinyu <xinyu@test.com>
2023-06-07 15:04:27 +0800

    新增

--- a982052-linebot/LineBotApp.cs
+++ a982052-linebot/LineBotApp.cs
@@ -1,9 +1,40 @@
-﻿namespace a982052_linebot;
+﻿using Line.Messaging;
+using Line.Messaging.Webhooks;
 
-public class LineBotApp
+namespace a982052_linebot;
+
+public class LineBotApp : WebhookApplication
 {
-    public async Task RunAsync(object events)
+    private readonly LineMessagingClient _messagingClient;
+    public LineBotApp(LineMessagingClient lineMessagingClient)
     {
-        throw new NotImplementedException();
+        _messagingClient = lineMessagingClient;
+    }
+
+    protected override async Task OnMessageAsync(MessageEvent ev)
+    {
+        var result = null as List<ISendMessage>;
+
+        switch (ev.Message)
+        {
+            //文字訊息
+            case TextEventMessage textMessage:
+            {
+                //頻道Id
+                var channelId = ev.Source.Id;
+                //使用者Id
+                var userId = ev.Source.UserId;
+                    
+                //回傳 hellow
+                result = new List<ISendMessage>
+                {
+                    new TextMessage("Hi Hi")
+                };
+            }
+                break;
+        }
+
+        if (result != null)
+            await _messagingClient.ReplyMessageAsync(ev.ReplyToken, result);
     }
 }
