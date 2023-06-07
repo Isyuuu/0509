7cc7e0e60aa231696f96b39a1f9be17dc9f58dff
xinyu <xinyu@test.com>
2023-06-07 15:23:20 +0800

    新增

--- a982052-linebot/Controllers/LineBotController.cs
+++ a982052-linebot/Controllers/LineBotController.cs
@@ -22,8 +22,8 @@ namespace a982052_linebot.Controllers
             _httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
             _httpContext = _httpContextAccessor.HttpContext;
             _lineBotConfig = new LineBotConfig();
-            _lineBotConfig.channelSecret = "";
-            _lineBotConfig.accessToken = "";
+            _lineBotConfig.channelSecret = "27367ec0201bc7431c67f1e7c3b18233";
+            _lineBotConfig.accessToken = "7YaHlqGIMXQALmT//CgJYcSfNIxIH4LtBiOaRnnvyF3VFtdoHlhZx35YyfNc8I72TmpExyALx8vin8xVWAAp6mN1dehIRA67aOXTX2lMFztqo37hdiZVhHleJVW9t20kRJHmMSPKGmx2lKNGaMJ6NAdB04t89/1O/w1cDnyilFU=";
         }
         
         //完整的路由網址就是 https://xxx/api/linebot/run
